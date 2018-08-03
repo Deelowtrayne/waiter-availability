ccdabc5b0550a4dbbcea8647c2111ffb295b118a
Luvuyo Sono <luvuyosono@gmail.com>
2018-08-03 11:06:44 +0200

    heroku setup

--- insert_data.sql
+++ insert_data.sql
@@ -18,23 +18,6 @@ INSERT INTO weekdays (day_name) VALUES ('Sunday');
 
 -- WorkShifts data
 
--- Mishy
-INSERT INTO shifts (user_id, weekday_id) VALUES (2, 1);
-INSERT INTO shifts (user_id, weekday_id) VALUES (2, 3);
-INSERT INTO shifts (user_id, weekday_id) VALUES (2, 5);
-INSERT INTO shifts (user_id, weekday_id) VALUES (2, 6);
-
--- Avis
-INSERT INTO shifts (user_id, weekday_id) VALUES (3, 1);
-INSERT INTO shifts (user_id, weekday_id) VALUES (3, 2);
-INSERT INTO shifts (user_id, weekday_id) VALUES (3, 4);
-INSERT INTO shifts (user_id, weekday_id) VALUES (3, 5);
-
--- MrBooi
-INSERT INTO shifts (user_id, weekday_id) VALUES (4, 1);
-INSERT INTO shifts (user_id, weekday_id) VALUES (4, 4);
-INSERT INTO shifts (user_id, weekday_id) VALUES (4, 6);
-INSERT INTO shifts (user_id, weekday_id) VALUES (4, 7);
 
 select * from users;
 select * from weekdays;
